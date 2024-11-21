ee652775c66bbca27e92129eb5f15949eb4ada87
ghtjd1358 <hojjang18@naver.com>
2024-11-21 20:58:39 +0900

    Test

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,7 @@
 import { authStatusType } from "@/shared/constants";
 import { MainArticleSlider } from "./components/MainArticleSlider";
 import { Layout } from "../common/components/Layout";
+import { Suspense } from "react";
 import { ApiErrorBoundary } from "../common/components/ApiErrorBoundary";
 import { MainProductList } from "./components/MainProductList";
 
@@ -10,9 +11,22 @@ export const HomePage: React.FC = () => {
       <div className="w-full bg-gray-800 text-gray-200">
         <MainArticleSlider />
         <ApiErrorBoundary>
-          <MainProductList />
+          <Suspense fallback={<LoadingSkeleton />}>
+            <MainProductList />
+          </Suspense>
         </ApiErrorBoundary>
       </div>
     </Layout>
   );
 };
+
+const LoadingSkeleton = () => (
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-32">
+    {[...Array(12)].map((_, index) => (
+      <div
+        key={index}
+        className="h-64 bg-gray-600 rounded-lg animate-pulse mb-32"
+      />
+    ))}
+  </div>
+);
