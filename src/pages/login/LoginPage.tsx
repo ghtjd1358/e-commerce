c2cc672ef3586d5baba10293c55af0a3a5b6e13d
ghtjd1358 <hojjang18@naver.com>
2024-12-17 21:20:48 +0900

    로고 수정

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -8,7 +8,7 @@ export const LoginPage: React.FC = () => {
       <div className="min-h-screen bg-gray-800 flex items-center justify-center p-4">
         <div className="w-full max-w-[470px] max-h-screen flex items-center p-5 border border-lightgray flex-col bg-gray-700 rounded-md">
           <h1 className="text-2xl font-bold text-yellow-400 my-4 tracking-widest">
-            이커머스
+          Dogun Dogun
           </h1>
           <LoginForm />
         </div>
